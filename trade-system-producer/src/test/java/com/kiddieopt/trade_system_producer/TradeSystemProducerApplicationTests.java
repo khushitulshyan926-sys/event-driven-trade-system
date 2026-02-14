91641b34a110e396e941be8cd4c13597f22c23a8
khushitulshyan926-sys <khushitulshyan926@gmail.com>
2026-02-15 02:34:32 +0530

    Debugging application while running

--- trade-system-producer/src/test/java/com/kiddieopt/trade_system_producer/TradeSystemProducerApplicationTests.java
+++ trade-system-producer/src/test/java/com/kiddieopt/trade_system_producer/TradeSystemProducerApplicationTests.java
@@ -1,8 +1,10 @@
 package com.kiddieopt.trade_system_producer;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class TradeSystemProducerApplicationTests {
 
